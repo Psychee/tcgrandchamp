93da2dfe773f3bdbdad094dc73c30d816bb4b1f4
Psychee <orelydbm@gamil.com>
2022-04-05 14:33:17 +0200

    ajout page pour la creation et l'affichage

---
+++ includes/affichageListeEquipe.php
@@ -0,0 +1,61 @@
+<?php 
+
+global $wpdb;
+$tablename = $wpdb->prefix."equipe";
+
+// supprimer un enregistrement
+if(isset($_GET['supprimer'])){
+	$supprimer = $_GET['supprimer'];
+	$wpdb->query("DELETE FROM ".$tablename." WHERE id=".$supprimer);
+}
+?>
+<h1>Liste des équipes</h1>
+
+<div class="wrap">
+  <button type = "button" class="btn btn-light btn-lg" id="listeEquipe" name ="listeEquipe" >Liste des équipes</button>
+  <button type = "button" class="btn btn-light btn-lg" id="parametrage" name ="Parametrage" >Paramétrage</button>
+  <p>Afficher la liste des equipes non archiver dans un tableau</p>
+</div>  
+
+<table width='100%' border='1' style='border-collapse: collapse;'>
+	<tr>
+		<th>id</th>
+		<th>Annee Sportive</th>
+        <th>Libelle équipe</th>
+		<th>Détails</th>
+		<th>&nbsp;</th>
+	</tr>
+	<?php
+	// Selectionner un enregistrement
+	$entreeListe = $wpdb->get_results("SELECT * FROM ".$tablename." order by id desc");
+	if(count($entreeListe) > 0){
+		$count = 1;
+		foreach($entreeListe as $entree){
+		    $id = $entree->id;
+		    $libelle = $entree->name;
+		    $annee = $entree->username;
+		    
+
+		    echo "<tr>
+		    	<td>".$id."</td>
+		    	<td>".$libelle."</td>
+		    	<td>".$annee."</td>
+		    	<td><a href='?page=allentries&delid=".$id."'>Delete</a></td>
+		    </tr>
+		    ";
+		    
+		}
+	}else{
+		echo "<tr><td colspan='5'>No record found</td></tr>";
+	}
+	
+
+	?>
+</table>
+
+
+
+
+
+  
+
---
+++ includes/creationEquipe.php
@@ -0,0 +1,70 @@
+<?php 
+
+global $wpdb;
+
+// ajouter une equipe
+if(isset($_POST['but_submit'])){
+
+	$libelle = $_POST['txt_libelle'];
+	$annee = $_POST['txt_annee'];
+	$numero_champ = $_POST['txt_numero_champ'];
+    $division_champ = $_POST['txt_division_champ'];
+    $phase_champ = $_POST['txt_phase_champ'];
+    $poule_champ = $_POST['txt_poule_champ'];
+    $equipe_champ = $_POST['txt_equipe_champ'];
+    $archiver = $_POST['txt_archiver'];
+
+	$tablename = $wpdb->prefix."equipe";
+
+	if($libelle != '' && $annee != '' && $numero_champ != ''&& $division_champ != '' && $phase_champ != ''  && $poule_champ != ''&& $equipe_champ != ''&& $archiver != ''){
+		$check_data = $wpdb->get_results("SELECT * FROM ".$tablename." WHERE libelle ='".$libelle."' ");
+	    if(count($check_data) == 0){
+	        $insert_sql = "INSERT INTO ".$tablename."(libelle,annee,numer_champ,division_champ,phase_champ,poule_champ,equipe_champ,archiver) values('".$libelle."','".$annee."','".$numero_champ."','".$division_champ."','".$phase_champ."','".$poule_champ."','".$equipe_champ."','".$archiver."') ";
+	        $wpdb->query($insert_sql);
+	        echo "l'equipe a ete creer avec succes!";
+	    }
+	}
+}
+
+?>
+<h1>Création Equipe</h1>
+<form method='post' action=''>
+	<table>
+		<tr>
+			<td>Libelle</td>
+			<td><input type='text' name='txt_libelle'></td>
+		</tr>
+		<tr>
+			<td>Année</td>
+			<td><input type='text' name='txt_annee'></td>
+		</tr>
+		<tr>
+			<td>Numéro de Championnat</td>
+			<td><input type='text' name='txt_numero_champ'></td>
+		</tr>
+        <tr>
+			<td>Division</td>
+			<td><input type='text' name='txt_division_champ'></td>
+		</tr>
+        <tr>
+			<td>Phase</td>
+			<td><input type='text' name='txt_phase_champ'></td>
+		</tr>
+        <tr>
+			<td>Poule</td>
+			<td><input type='text' name='txt_poule_champ'></td>
+		</tr>
+        <tr>
+			<td>Equipe</td>
+			<td><input type='text' name='txt_equipe_champ'></td>
+		</tr>
+        <tr>
+			<td>Archiver</td>
+			<td><input type='checkbox' name='txt_archiver'></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type='submit' name='but_submit' value='Enregister'></td>
+		</tr>
+	</table>
+</form>
